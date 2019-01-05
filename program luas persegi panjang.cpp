778cd88af5a2d8c288fc910fc8cfd29bfa764b9e
AminPriadi <45525407+AminPriadi@users.noreply.github.com>
2019-01-05 10:10:05 +0700

    Add files via upload

---
+++ program luas persegi panjang.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float P,L,luas;
+    cout<<"masukan nilai panjang ";
+    cin>>P;
+    cout<<"masukan nilai lebar ";
+    cin>>L;
+
+    luas=P*L;
+    cout<<"maka luas persegi panjang adalah ";
+    cout<<luas;
+
+    return 0;
+}
